7e0d49a41a7840de681ace2f8645b4180e8e2c9c
Fernando Belen <fernando.belen@naranjax.com>
2021-09-29 10:18:10 -0300

    Initial Commit

---
+++ MVCApiUITests/MVCApiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVCApiUITestsLaunchTests.swift
+//  MVCApiUITests
+//
+//  Created by Fernado Belen on 29/09/2021.
+//
+
+import XCTest
+
+class MVCApiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
